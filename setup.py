d4a441baed3b742ae149e3b83ede1cceb2c7372f
Arjan Scherpenisse <arjan@scherpenisse.net>
2011-04-26 09:26:37 +0200

    Added pypi

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Copyright (c) 2010 Arjan Scherpenisse
+# See LICENSE for details.
+
+"""
+WeAreForests installation script
+"""
+
+from setuptools import setup
+import os
+import sys
+import weareforests
+from twisted.python import procutils
+
+
+setup(
+    name = "WeAreForests",
+    version = weareforests.__version__,
+    author = "Arjan Scherpenisse",
+    author_email = "arjan@scherpenisse.net",
+    url = "http://scherpenisse.net/weareforests",
+    description = "Telephony-based sound performance",
+    packages = ['weareforests'],
+    install_requires = [
+    'Sparked>=0.10'
+    ],
+    classifiers = [
+        "Framework :: Twisted",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Communications",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
+        ]
+    )
+
